681e4963ced0230f30220f9f2dd1e58f436c2067
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-06-21 19:30:20 +0800

    Sell stock solutions

--- leetcode/DynamicProgramming/sell_stock/best_time_to_buy_and_sell_stock_transaction_fee.cc
+++ leetcode/DynamicProgramming/sell_stock/best_time_to_buy_and_sell_stock_transaction_fee.cc
@@ -8,6 +8,24 @@
 using std::vector;
 using std::max;
 
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int len = prices.size();
+        if (len == 0) return 0;
+        vector<vector<int>> dp(len, vector<int>(2, 0));
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        for(int i = 1; i < len; ++i) {
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i]);
+            dp[i][1] = max(dp[i-1][0] + prices[i] - fee, dp[i-1][1]); 
+        }
+        return dp[len-1][1];
+
+    }
+};
+
 class Solution {
  public:
   /*

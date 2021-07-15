ee9e993417e8144c317cc28cf6cb41ed2d70a4a9
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-07-15 22:58:17 +0800

    add leetcode solutions

---
+++ leetcode/Tree/maximum_depth_of_n_ary_tree.cc
@@ -0,0 +1,72 @@
+#include <vector>
+#include <stack>
+
+#include "./tree_node.h"
+
+#include "glog/logging.h"
+#include "gflags/gflags.h"
+
+using namespace std;
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (root == nullptr) return 0;
+        int result = 0;
+        for (int i = 0; i < root->children.size(); ++i) {
+            result = max(result, maxDepth(root->children[i]));
+        }
+        return result+1;
+    }
+};
+
+class Solution1 {
+public:
+    int maxDepth(Node* root) {
+        if (root == nullptr) return 0;
+       queue<Node*> Nodes;
+       Nodes.push(root);
+       int depth = 0;
+       while (!Nodes.empty()) {
+           int size = Nodes.size();
+           for (int i = 0; i < size; ++i) {
+               Node* node = Nodes.front();
+               Nodes.pop();
+               for (int j = 0; j < node->children.size(); ++j) {
+                   if (node->children[j]) Nodes.push(node->children[j]);
+               }
+           }
+           depth += 1;
+       }
+       return depth;
+
+    }
+};
+
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+  Solution solu;
+  return 0;
+}
--- leetcode/Tree/minimum_depth_of_binary_tree.cc
+++ leetcode/Tree/minimum_depth_of_binary_tree.cc
@@ -1,8 +1,6 @@
 #include <vector>
 #include <stack>
 
-#include <limits.h>
-
 #include "./tree_node.h"
 
 #include "glog/logging.h"
@@ -10,27 +8,69 @@
 
 using namespace std;
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
- public:
-  int minDepth(TreeNode* root) {
-    if (root == nullptr) {
-      return 0;
-    }
-  
-    if ((root->left == nullptr) && (root->right == nullptr)) {
-      return 1;
-    }
-  
-    int min_depth = INT_MAX;
-    if (root->left != nullptr) {
-      min_depth = min(minDepth(root->left), min_depth);
-    }
-    if (root->right != nullptr) {
-      min_depth = min(minDepth(root->right), min_depth);
+private:
+    int getDepth(TreeNode* node) {
+        if (node == nullptr) return 0;
+
+        int depth = 0;
+        int left_depth = getDepth(node->left);
+        int right_depth = getDepth(node->right);
+        // 右子树正常，左子树为空，非最低
+        if (node->left == nullptr && node->right != nullptr) {
+            return right_depth + 1;
+        }
+        // 左子树正常，右子树为空，非最低
+        if (node->left != nullptr && node->right == nullptr) {
+            return left_depth + 1;
+        }
+
+        return 1+min(left_depth, right_depth);
+
     }
 
-    return min_depth + 1;
-  }
+public:
+    int minDepth(TreeNode* root) {
+        int result = getDepth(root);
+        return result;
+    }
+};
+
+class Solution1 {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int depth = 0;
+
+        queue<TreeNode*> Nodes;
+        Nodes.push(root);
+        while(!Nodes.empty()) {
+            int size = Nodes.size();
+            depth += 1;
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = Nodes.front();
+                Nodes.pop();
+                if (node->left) Nodes.push(node->left);
+                if (node->right) Nodes.push(node->right);
+                if (node->left == nullptr && node->right == nullptr) {
+                    return depth;
+                }
+            }
+        }
+        return depth;
+
+    }
 };
 
 int main(int argc, char* argv[]) {

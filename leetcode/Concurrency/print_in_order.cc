2426e4a497194875f80ada09d2e0c80f8940b20a
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-08-12 22:19:04 +0800

    add leetcode solutions

---
+++ leetcode/Concurrency/print_in_order.cc
@@ -0,0 +1,62 @@
+#include <vector>
+#include <limits.h>
+#include <algorithm>
+#include <mutex>
+#include <memory>
+#include <condition_variable>
+
+// #include "glog/logging.h"
+// #include "gflags/gflags.h"
+
+using std::vector;
+using std::min;
+
+class Foo {
+private:
+    std::mutex first_mux_, second_mux_;
+    std::condition_variable first_con_, second_con_;
+    bool first_flag_, second_flag_;
+    
+public:
+    Foo() {
+        first_flag_ = false;
+        second_flag_ = false;
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        std::unique_lock<mutex> lk(first_mux_);
+        first_flag_ = true;
+        first_con_.notify_one();
+    }
+
+    void second(function<void()> printSecond) {
+        {
+            std::unique_lock<mutex> lk(first_mux_);
+            first_con_.wait(lk, [this]{return first_flag_;});
+        }
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        std::unique_lock<mutex> lk(second_mux_);
+        second_flag_ = true;
+        second_con_.notify_one();
+    }
+
+    void third(function<void()> printThird) {
+        {
+            std::unique_lock<mutex> lk(second_mux_);
+            second_con_.wait(lk, [this]{return second_flag_;});
+        }
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+  Solution solu;
+  return 0;
+}

e9ec17ebe08d934341fadcb44e3c6ac0e4ac1849
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-08-21 11:19:53 +0800

    add leetcode RotatedSortedArray

---
+++ leetcode/BinarySearch/RotatedSortedArray/find_minimum_in_rotated_sorted_array_ii.cc
@@ -0,0 +1,41 @@
+#include <vector>
+#include <assert.h>
+#include <algorithm>
+
+#include "glog/logging.h"
+#include "gflags/gflags.h"
+
+using std::vector;
+using std::min;
+
+class Solution {
+ public:
+  int findMin(vector<int>& nums) {
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] < nums[right]) {
+        right = mid;
+      }
+      else if (nums[mid] > nums[right]) {
+        left = mid + 1;
+      }
+      else {
+        right -= 1;
+      }
+    }
+    return nums[left];
+  }
+};
+
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+  Solution solu;
+  vector<int> nums = {3,4,5,1,2};
+  int ret = solu.findMin(nums);
+  LOG(INFO) << ret;
+  return 0;
+}
--- leetcode/TwoPointers/next_permutation.cc
+++ leetcode/TwoPointers/next_permutation.cc
@@ -10,25 +10,30 @@ using namespace std;
 // Problem: https://leetcode-cn.com/problems/next-permutation
 // Solution: https://leetcode-cn.com/problems/next-permutation/solution/xia-yi-ge-pai-lie-suan-fa-xiang-jie-si-lu-tui-dao-/
 
-// TowPointer.
 class Solution {
- public:
-  void nextPermutation(vector<int>& nums) {
-      int i = nums.size() - 2;
-      while (i >= 0 && nums[i] >= nums[i + 1]) {
-          i--;
-      }
-      if (i >= 0) {
-          int j = nums.size() - 1;
-          while (j >= 0 && nums[i] >= nums[j]) {
-              j--;
-          }
-          swap(nums[i], nums[j]);
-      }
-      reverse(nums.begin() + i + 1, nums.end());
-  }
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i = nums.size() - 2;
+        // 在尽可能靠右的低位进行交换，需要从后向前查找
+        // 将一个 尽可能小的「大数」 与前面的「小数」交换
+        // 将「大数」换到前面后，需要将「大数」后面的所有数重置为升序，升序排列就是最小的排列
+        // 从最右边找较大值
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.size() - 1;
+            // 从最右边找较小值
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            // 交换
+            swap(nums[i], nums[j]);
+        }
+        // 
+        reverse(nums.begin() + i + 1, nums.end());
+    }
 };
-
 
 int main(int argc, char* argv[]) {
   // google::InitGoogleLogging(argv[0]);

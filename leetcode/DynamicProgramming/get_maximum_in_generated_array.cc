a64690f33c5636709abba06d6cdc9b0c775a8487
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-08-23 21:01:44 +0800

    add leetcode solutions

---
+++ leetcode/DynamicProgramming/get_maximum_in_generated_array.cc
@@ -0,0 +1,38 @@
+#include <vector>
+#include <limits.h>
+#include <algorithm>
+
+// #include "glog/logging.h"
+// #include "gflags/gflags.h"
+
+using std::vector;
+using std::min;
+
+class Solution {
+ public:
+  int getMaximumGenerated(int n) {
+      if (n <= 1) return n;
+      vector<int> dp(n+1, 0);
+      dp[0] = 0;
+      dp[1] = 1;
+      int max_value = dp[1];
+      for (int k = 2; k <= n; ++k) {
+          if (k % 2 == 0) {
+              int i = k / 2;
+              dp[k] = dp[i];
+          } else {
+              int i = (k - 1)/2;
+              dp[k] = dp[i] + dp[i+1];
+          }
+          if (dp[k] > max_value) max_value = dp[k];
+      }
+      return max_value;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+  Solution solu;
+  return 0;
+}

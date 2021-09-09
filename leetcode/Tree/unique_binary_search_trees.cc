2e1808a44c8efdc25f3cae07727a832f9574f4d4
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-09-09 23:46:19 +0800

    add leetcode solutions

---
+++ leetcode/Tree/unique_binary_search_trees.cc
@@ -0,0 +1,46 @@
+#include <vector>
+#include <cstdlib>
+#include <random>
+
+#include "third_party/glog/include/logging.h"
+#include "third_party/gflags/include/gflags.h"
+
+using namespace std;
+// Problems: https://leetcode-cn.com/problems/unique-binary-search-trees/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> G(n + 1, 0);
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+};
+
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+  Solution solu;
+  return 0;
+}

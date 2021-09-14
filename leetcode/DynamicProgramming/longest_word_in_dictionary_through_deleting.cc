e8d18c60186645c8318b92e17284e3e958bee205
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-09-14 23:36:10 +0800

    add leetcode solutions

---
+++ leetcode/DynamicProgramming/longest_word_in_dictionary_through_deleting.cc
@@ -0,0 +1,47 @@
+#include <vector>
+#include <limits.h>
+#include <string>
+#include <algorithm>
+
+#include "third_party/glog/include/logging.h"
+#include "third_party/gflags/include/gflags.h"
+
+using std::vector;
+using std::string;
+using std::min;
+
+// Problem: https://leetcode-cn.com/problems/longest-word-in-dictionary-through-deleting/
+
+class Solution {
+ public:
+    static bool cmp(string &a,string &b){
+        if(a.size()==b.size()) return a<b;
+        return a.size()>b.size();
+    }
+    string findLongestWord(string s, vector<string>& dictionary) {
+        std::sort(dictionary.begin(),dictionary.end(),cmp);
+        int len=s.size();
+        for(auto &c : dictionary){
+            int left=0,right=0;
+            int lenc=c.size();
+            for(; left<len && right<lenc; left++){
+                if(s[left] == c[right]){
+                    right++;
+                }
+            }
+            if(right == lenc) return c;
+        }
+        return "";
+    }
+};
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+  Solution solu;
+  string s("abpcplea");
+  vector<string> dictionary({"ale","apple","monkey","plea"});
+  string ret = solu.findLongestWord(s, dictionary);
+  LOG(INFO) << ret ;
+  return 0;
+}

b4188b6450991295912e0ab0faaf476dc9ae6198
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-09-21 11:19:58 +0800

    modify format

--- leetcode/Array/valid_sudoku.cc
+++ leetcode/Array/valid_sudoku.cc
@@ -1,68 +1,68 @@
-#include <vector>
 #include <set>
+#include <vector>
 
-#include "third_party/glog/include/logging.h"
 #include "third_party/gflags/include/gflags.h"
-
+#include "third_party/glog/include/logging.h"
 
 using namespace std;
 
 class Solution {
-private:
-    bool isValidCol(vector<vector<char>>& board) {
-        set<int> table;
-        for (int col = 0; col < board.size(); ++ col) {
-            table.clear();
-            for (int i = 0; i < board[col].size(); ++i) {
-                if (board[col][i] == '.') continue;
-                if (table.count(board[col][i] - '0') != 0) {
-                    return false;
-                }
-                table.insert(board[col][i] - '0');
-            }
+ private:
+  bool isValidCol(vector<vector<char>>& board) {
+    set<int> table;
+    for (int col = 0; col < board.size(); ++col) {
+      table.clear();
+      for (int i = 0; i < board[col].size(); ++i) {
+        if (board[col][i] == '.') continue;
+        if (table.count(board[col][i] - '0') != 0) {
+          return false;
         }
-        return true;
+        table.insert(board[col][i] - '0');
+      }
     }
-    bool isValidRow(vector<vector<char>>& board) {
-        set<int> table;
-        for (int row = 0; row < board.size(); ++ row) {
-            table.clear();
-            for (int i = 0; i < board.size(); ++i) {
-                if (board[i][row] == '.') continue;
-                if (table.count(board[i][row] - '0') != 0) {
-                    return false;
-                }
-                table.insert(board[i][row] - '0');
-            }
+    return true;
+  }
+  bool isValidRow(vector<vector<char>>& board) {
+    set<int> table;
+    for (int row = 0; row < board.size(); ++row) {
+      table.clear();
+      for (int i = 0; i < board.size(); ++i) {
+        if (board[i][row] == '.') continue;
+        if (table.count(board[i][row] - '0') != 0) {
+          return false;
         }
-        return true;
+        table.insert(board[i][row] - '0');
+      }
     }
-    bool isValidSub(vector<vector<char>>& board, int pos, int row) {
-        set<int> table;
-        for (int i = pos; i < pos+3; ++i) {
-            for (int j = row; j < row + 3; ++j) {
-                if (board[i][j] == '.') continue;
-                if (table.count(board[i][j] - '0') != 0) {
-                    return false;
-                }
-                table.insert(board[i][j] - '0');
-            }
+    return true;
+  }
+  bool isValidSub(vector<vector<char>>& board, int pos, int row) {
+    set<int> table;
+    for (int i = pos; i < pos + 3; ++i) {
+      for (int j = row; j < row + 3; ++j) {
+        if (board[i][j] == '.') continue;
+        if (table.count(board[i][j] - '0') != 0) {
+          return false;
         }
-        return true;
+        table.insert(board[i][j] - '0');
+      }
     }
-public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        bool col_ret = isValidCol(board);
-        bool row_ret = isValidRow(board);
-        if (!col_ret || !row_ret) return false;
-        for (int i = 0; i < board.size(); i = i+3) {
-            for (int j = 0; j < board.back().size(); j = j+3) {
-                bool ret = isValidSub(board, i, j);
-                if (!ret) return false;
-            }
-        }
-        return true;
+    return true;
+  }
+
+ public:
+  bool isValidSudoku(vector<vector<char>>& board) {
+    bool col_ret = isValidCol(board);
+    bool row_ret = isValidRow(board);
+    if (!col_ret || !row_ret) return false;
+    for (int i = 0; i < board.size(); i = i + 3) {
+      for (int j = 0; j < board.back().size(); j = j + 3) {
+        bool ret = isValidSub(board, i, j);
+        if (!ret) return false;
+      }
     }
+    return true;
+  }
 };
 
 int main(int argc, char* argv[]) {

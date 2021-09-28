f6f6cf0af5c0c5baaed2286aea64681b1d7805d2
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-09-28 23:12:31 +0800

    add leetcode solutions

---
+++ leetcode/DFS/path_sum_iii.cc
@@ -0,0 +1,73 @@
+#include <vector>
+
+#include "third_party/gflags/include/gflags.h"
+#include "third_party/glog/include/logging.h"
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  map<int, int> mp;
+  int target, ans;
+  int pathSum(TreeNode *root, int targetSum) {
+    target = targetSum;
+    mp[0] = 1;
+    dfs(root, 0);
+    return ans;
+  }
+
+  void dfs(TreeNode *node, int sum) {
+    if (node == NULL) return;
+    sum += node->val;
+    ans += mp[sum - target];
+    if (mp.find(sum) != mp.end())
+      mp[sum]++;
+    else
+      mp[sum] = 1;
+    dfs(node->left, sum);
+    dfs(node->right, sum);
+    mp[sum]--;
+  }
+};
+
+class Solution1 {
+ public:
+  int target, ans;
+  int pathSum(TreeNode *root, int targetSum) {
+    if (root == NULL) return 0;
+    target = targetSum;
+    dfs(root);
+    return ans;
+  }
+
+  void dfs(TreeNode *node) {
+    dfs2(node, 0);
+    if (node->left != NULL) dfs(node->left);
+    if (node->right != NULL) dfs(node->right);
+  }
+
+  void dfs2(TreeNode *node, int sum) {
+    sum += node->val;
+    if (sum == target) ++ans;
+    if (node->left != NULL) dfs2(node->left, sum);
+    if (node->right != NULL) dfs2(node->right, sum);
+  }
+};
+
+int main(int argc, char *argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+  Solution solu;
+  int ret = 0;
+  return 0;
+}

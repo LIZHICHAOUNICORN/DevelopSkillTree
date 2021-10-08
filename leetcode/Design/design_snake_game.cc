29962e173ba2e170cf4ddc03a786621b69514f0d
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-10-08 23:30:37 +0800

    add leetcode solutions

---
+++ leetcode/Design/design_snake_game.cc
@@ -0,0 +1,99 @@
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <deque>
+#include <vector>
+
+#include "third_party/gflags/include/gflags.h"
+#include "third_party/glog/include/logging.h"
+
+using namespace std;
+
+// Problem: https://leetcode-cn.com/problems/lru-cache/
+class SnakeGame {
+ public:
+  /** Initialize your data structure here.
+      @param width - screen width
+      @param height - screen height
+      @param food - A list of food positions
+      E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the
+     second is at [1,0]. */
+  SnakeGame(int width, int height, vector<vector<int>>& food) {
+    this->width = width;
+    this->height = height;
+    this->food = food;
+    dq.push_back(make_pair(0, 0));
+    end = false;
+    index = 0;
+  }
+
+  /** Moves the snake.
+      @param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down
+      @return The game's score after the move. Return -1 if game over.
+      Game over when snake crosses the screen boundary or bites its body. */
+  int move(string direction) {
+    // 如果之前有吃到自己或者撞到墙壁，那么就直接返回-1
+    if (end) return -1;
+    // 取出头部位置并按照要求去进行变化
+    pair<int, int> cur = dq.front();
+    if (direction == "U")
+      cur.first--;
+    else if (direction == "D")
+      cur.first++;
+    else if (direction == "L")
+      cur.second--;
+    else if (direction == "R")
+      cur.second++;
+    // 如果撞墙
+    if (cur.first < 0 || cur.first >= height || cur.second < 0 ||
+        cur.second >= width) {
+      end = true;
+      return -1;
+    }
+    // 如果吃到自己,注意尾部最后一个节点不算
+    for (auto iter = dq.begin(); iter != dq.end() - 1; iter++) {
+      if ((*iter).first == cur.first && (*iter).second == cur.second) {
+        end = true;
+        return -1;
+      }
+    }
+    // 如果没有食物了,就直接返回,记得蛇位置也要移动一下
+    if (index == food.size()) {
+      dq.push_front(cur);
+      dq.pop_back();
+      return index;
+    }
+    // 头部插入新的位置
+    dq.push_front(cur);
+    // 如果吃到食物,身体就增长1个,增长的就是尾部位置
+    if (cur.first == food[index][0] && cur.second == food[index][1]) {
+      index++;
+    }
+    // 如果没有吃到食物,最后一个位置更新一下
+    else {
+      dq.pop_back();
+    }
+    return index;
+  }
+
+ private:
+  deque<pair<int, int>> dq;
+  int width, height;
+  vector<vector<int>> food;
+  bool end;
+  int index;
+};
+
+/**
+ * Your SnakeGame object will be instantiated and called as such:
+ * SnakeGame* obj = new SnakeGame(width, height, food);
+ * int param_1 = obj->move(direction);
+ */
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+
+  return 0;
+}
---
+++ leetcode/HashTable/repeated_dna_sequences.cc
@@ -0,0 +1,40 @@
+#include <string>
+#include <unordered_map>
+
+#include "third_party/gflags/include/gflags.h"
+#include "third_party/glog/include/logging.h"
+
+// Problems: https://leetcode-cn.com/problems/repeated-dna-sequences/
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<string> findRepeatedDnaSequences(string s) {
+    vector<string> ret;
+    if (s.size() <= 10) return ret;
+
+    unordered_map<string, int> cache;
+
+    for (size_t i = 0; i <= s.size() - 10; ++i) {
+      const string& sub = s.substr(i, 10);
+      if (cache.count(sub) > 0 && cache[sub] == 1) {
+        ret.push_back(sub);
+      }
+      ++cache[sub];
+    }
+    return ret;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+  string test_case("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT");
+
+  Solution solu;
+  auto ret = solu.findRepeatedDnaSequences(test_case);
+  LOG(INFO) << ret.back();
+  return 0;
+}

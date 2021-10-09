efa093654ecfafe413d91de2c6137c20f3e378c6
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-10-09 13:55:11 +0800

    add codelab memory

---
+++ codelab/cplusplus11/memory/mem_pack.cc
@@ -0,0 +1,31 @@
+/*
+说明：程序是在 64 位编译器下测试的
+*/
+#include <iostream>
+
+using namespace std;
+
+struct A {
+    short var; // 2 字节
+    int var1;  // 8 字节 （内存对齐原则：填充 2 个字节） 2 (short) + 2 (填充) + 4 (int)= 8
+    long var2; // 12 字节 8 + 4 (long) = 12
+    char var3; // 16 字节 （内存对齐原则：填充 3 个字节）12 + 1 (char) + 3 (填充) = 16
+    string s;  // 48 字节 16 + 32 (string) = 48
+};
+
+int main(int argc, char * argv[]) {
+    short var;
+    int var1;
+    long var2;
+    char var3;
+    string s;
+    A ex1;
+    cout << sizeof(var) << endl;  // 2 short
+    cout << sizeof(var1) << endl; // 4 int
+    cout << sizeof(var2) << endl; // 4 long
+    cout << sizeof(var3) << endl; // 1 char
+    cout << sizeof(s) << endl;    // 32 string
+    cout << sizeof(ex1) << endl;  // 48 struct
+    return 0;
+}
+

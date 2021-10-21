1a8c1940b972ce69962c7ceefe5159690cfc1fdc
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-10-21 21:28:58 +0800

    add leetcode solutions

---
+++ leetcode/Array/task_scheduler.cc
@@ -0,0 +1,46 @@
+#include <set>
+#include <vector>
+
+#include "third_party/gflags/include/gflags.h"
+#include "third_party/glog/include/logging.h"
+
+// #include <gperftools/profiler.h>
+// Problem: https://leetcode-cn.com/problems/task-scheduler/
+
+using namespace std;
+
+class Solution {
+ public:
+  int leastInterval(vector<char>& tasks, int n) {
+    unordered_map<char, int> freq;
+    for (char ch : tasks) {
+      ++freq[ch];
+    }
+
+    // 最多的执行次数
+    int maxExec =
+        max_element(freq.begin(), freq.end(), [](const auto& u, const auto& v) {
+          return u.second < v.second;
+        })->second;
+    // 具有最多执行次数的任务数量
+    int maxCount = accumulate(
+        freq.begin(), freq.end(), 0,
+        [=](int acc, const auto& u) { return acc + (u.second == maxExec); });
+
+    return max((maxExec - 1) * (n + 1) + maxCount,
+               static_cast<int>(tasks.size()));
+  }
+};
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+  vector<char> tasks({"A", "A", "A", "B", "B", "B"});
+
+  Solution solu;
+  int ret = solu.leastInterval(tasks, 2);
+  LOG(INFO) << ret;
+
+  return 0;
+}
---
+++ leetcode/TwoPointers/shortest_unsorted_continuous_subarray.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "third_party/gflags/include/gflags.h"
+#include "third_party/glog/include/logging.h"
+
+using std::vector;
+using std::string;
+
+// Problem:
+// https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/
+
+class Solution {
+ public:
+  int findUnsortedSubarray(vector<int>& nums) {
+    if (is_sorted(nums.begin(), nums.end())) {
+      return 0;
+    }
+    vector<int> numsSorted(nums);
+    sort(numsSorted.begin(), numsSorted.end());
+    int left = 0;
+    while (nums[left] == numsSorted[left]) {
+      left++;
+    }
+    int right = nums.size() - 1;
+    while (nums[right] == numsSorted[right]) {
+      right--;
+    }
+    return right - left + 1;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+  vector<int> nums({2, 6, 4, 8, 10, 9, 15});
+
+  Solution solu;
+  int ret = solu.findUnsortedSubarray(nums);
+  LOG(INFO) << "ret " << ret;
+  return 0;
+}

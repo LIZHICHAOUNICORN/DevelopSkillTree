1be4d25ab3a43c9d9e032589ce749e028f6ad97a
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-10-25 14:11:25 +0800

    add leetcode solutions

---
+++ leetcode/Math/construct_the_rectangle.cc
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <climits>
+#include <vector>
+#include <cmath>
+
+#include "third_party/gflags/include/gflags.h"
+#include "third_party/glog/include/logging.h"
+
+using namespace std;
+
+// Problem:
+// https://leetcode-cn.com/problems/construct-the-rectangle/
+
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int w = sqrt(1.0 * area);
+        while (area % w) {
+            --w;
+        }
+        return {area / w, w};
+    }
+};
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+  Solution solu;
+  vector<int> ret = solu.constructRectangle(4);
+  for (const int & r : ret) {
+    LOG(INFO) << r;
+  }
+
+  return 0;
+}

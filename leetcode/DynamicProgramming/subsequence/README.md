6cdf9c380e0dd023f25bf83f2a3dd742e50ad14d
LIZHICHAOUNICORN Name <1072526820@qq.com>
2021-11-16 15:59:31 +0800

    add leetcode solutions

--- leetcode/DynamicProgramming/subsequence/README.md
+++ leetcode/DynamicProgramming/subsequence/README.md
@@ -5,17 +5,33 @@
 2. [1143.最长公共子序列](https://leetcode-cn.com/problems/longest-common-subsequence)    
 3. [1035.不相交的线](https://leetcode-cn.com/problems/uncrossed-lines)  
 
+- [x] longest-increasing-subsequence
+- [x] longest-common-subsequence
+- [x] uncrossed-lines
+
 ## 子序列连续问题
 1. [674. 最长连续递增序列](https://leetcode-cn.com/problems/longest-continuous-increasing-subsequence)  
 2. [718. 最长重复子数组](https://leetcode-cn.com/problems/maximum-length-of-repeated-subarray)  
 3. [53. 最大子序和](https://leetcode-cn.com/problems/maximum-subarray)  
+
+- [x] longest-continuous-increasing-subsequence
+- [x] maximum-length-of-repeated-subarray
+- [x] maximum-subarray
 
 ## 编辑距离问题
 1. [392. 判断子序列](https://leetcode-cn.com/problems/is-subsequence)    
 2. [115. 不同的子序列](https://leetcode-cn.com/problems/distinct-subsequences)    
 3. [583. 两个字符串的删除操作](https://leetcode-cn.com/problems/delete-operation-for-two-strings)    
 4. [72. 编辑距离](https://leetcode-cn.com/problems/edit-distance)    
+
+- [x] is-subsequence
+- [x] distinct-subsequences
+- [x] delete-operation-for-two-strings
+- [x] edit-distance
+
 ## 回文
 1. [647. 回文子串](https://leetcode-cn.com/problems/palindromic-substrings)    
 2. [516. 最长回文子序列](https://leetcode-cn.com/problems/longest-palindromic-subsequence)  
 
+- [x] palindromic-substrings
+- [x] longest-palindromic-subsequence
--- leetcode/DynamicProgramming/subsequence/distinct_subsequences.cc
+++ leetcode/DynamicProgramming/subsequence/distinct_subsequences.cc
@@ -7,11 +7,65 @@
 #include "third_party/glog/include/logging.h"
 
 // Problem: https://leetcode-cn.com/problems/distinct-subsequences/
+// Solutions:
+// https://leetcode-cn.com/problems/distinct-subsequences/solution/c-dfs-ji-yi-hua-sou-suo-dong-tai-gui-hua-m6e9/
 
 using std::vector;
 using std::string;
 
+// 暴力搜索
 class Solution {
+ public:
+  int dfs(string s, int index, string t, int cur) {
+    // 遍历t，走到最终结束
+    if (cur == t.size()) return 1;
+
+    // 遍历s，走到最终，但目前t还没有，所以没有匹配成功
+    if (index == s.size()) return 0;
+
+    // 如果当前字符相等, 那么t可选择匹配当前字符，t也可以选择不匹配当前字符.
+    if (s[index] == t[cur]) {
+      return dfs(s, index + 1, t, cur + 1) + dfs(s, index + 1, t, cur);
+    } else {
+      // 因为不相等，所以，一定不匹配当前字符。
+      return dfs(s, index + 1, t, cur);
+    }
+  }
+
+  int numDistinct(string s, string t) { return dfs(s, 0, t, 0); }
+};
+
+// 加cache
+class Solution1 {
+ public:
+  vector<vector<int>> arr;
+  int dfs(string s, int index, string t, int cur) {
+    // 遍历t，走到最终结束
+    if (cur == t.size()) return 1;
+
+    // 遍历s，走到最终，但目前t还没有，所以没有匹配成功
+    if (index == s.size()) return 0;
+
+    if (arr[cur][index] != -1) return arr[cur][index];
+
+    if (s[index] == t[cur]) {
+      arr[cur][index] =
+          dfs(s, index + 1, t, cur + 1) + dfs(s, index + 1, t, cur);
+    } else {
+      arr[cur][index] = dfs(s, index + 1, t, cur);
+    }
+
+    return arr[cur][index];
+  }
+
+  int numDistinct(string s, string t) {
+    int n = s.size(), m = t.size();
+    arr = vector<vector<int>>(m, vector<int>(n, -1));
+    return dfs(s, 0, t, 0);
+  }
+};
+
+class Solution2 {
  public:
   int numDistinct(string s, string t) {
     vector<vector<uint64_t>> dp(s.size() + 1, vector<uint64_t>(t.size() + 1));

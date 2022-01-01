ed211011312e8e946d7d225cf693d2ca8da72792
LIZHICHAOUNICORN Name <1072526820@qq.com>
2022-01-01 22:34:14 +0800

    add leetcode solutions

--- leetcode/Math/number_of_digit_one.cc
+++ leetcode/Math/number_of_digit_one.cc
@@ -1,13 +1,16 @@
 #include <stack>
+#include <cmath>
 #include <vector>
 
-// #include "third_party/glog/include/logging.h"
-// #include "third_party/gflags/include/gflags.h"
+#include "third_party/gflags/include/gflags.h"
+#include "third_party/glog/include/logging.h"
 
 using namespace std;
 
-// 作者：ningfan
-// 链接：https://leetcode-cn.com/problems/number-of-digit-one/solution/zhao-ge-gui-lu-qing-song-de-chu-da-an-hu-p6l6/
+// Problem: https://leetcode-cn.com/problems/number-of-digit-one/
+// Solution:
+// https://leetcode-cn.com/problems/number-of-digit-one/solution/zhao-ge-gui-lu-qing-song-de-chu-da-an-hu-p6l6/
+// https://leetcode-cn.com/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof/solution/mian-shi-ti-43-1n-zheng-shu-zhong-1-chu-xian-de-2/
 
 class Solution {
  public:
@@ -29,10 +32,34 @@ class Solution {
   }
 };
 
+class Solution1 {
+ public:
+  int countDigitOne(int n) {
+    int digit = 1, res = 0;
+    int high = n / 10, cur = n % 10, low = 0;
+    while (high != 0 || cur != 0) {
+      if (cur == 0)
+        res += high * digit;
+      else if (cur == 1)
+        res += high * digit + low + 1;
+      else
+        res += (high + 1) * digit;
+      low += cur * digit;
+      cur = high % 10;
+      high /= 10;
+      digit *= 10;
+    }
+    return res;
+  }
+};
+
+
 int main(int argc, char* argv[]) {
   google::InitGoogleLogging(argv[0]);
   gflags::ParseCommandLineFlags(&argc, &argv, false);
   Solution solu;
+  int ret = solu.countDigitOne(2341);
+  LOG(INFO) << ret;
 
   return 0;
 }
